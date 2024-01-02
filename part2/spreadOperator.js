50218d37672df53f9bfc62b8aa378b67b688ac55
pr642 <pr642@kali>
2024-01-02 20:38:14 +0100

    commiting the destrucutre

--- part2/spreadOperator.js
+++ part2/spreadOperator.js
@@ -16,3 +16,58 @@
  const anotherStrNums = ['four', 'five', 'six'];
  const main = [...strNums, ...anotherStrNums]
  console.log(main);
+
+ //Object with spread operator
+ const obj1 = {x:12, y:13, z:14};
+ const obj2 = {h:12, f:13, k:14};
+ const obj3 = {...obj1, ...obj2};// it tends to give you access inside the object
+ const obj4 = {obj1, obj2};
+ console.log(obj3);
+ console.log(obj4);
+ 
+ //class work
+ console.log("\n Class work\n");
+
+ // 1
+ let arr = [1,2,3];
+ let arr2 = [4,5];
+ let arr3 = [...arr, ...arr2];
+ console.log(arr3);
+
+ const user ={
+   name: "Jen",
+   age: 22
+ }
+
+const clone ={...user, country : "Nigeria"};
+
+console.log(clone);
+
+//rest operator, it is a parameter syntax that allows a function to accept an indefinte number of arguements, as an array, providing a way to represent variadic functioons in js, and it hass to be the last in a paramenter list
+
+// Variadic Functions are function that can recieve unlimited amount of parameters
+
+function userProfile(x,...userData){
+   console.log(x);
+   console.log(userData);
+}
+
+userProfile("PRAISE", 19, "Programming", "football");
+
+function person(firstName, lastName, ...Hobbies) {
+   console.log("FIrst Name: ", firstName);
+   console.log("Last Name: ", lastName);
+   console.log("Hobbies: ", Hobbies);
+}
+
+person("Praise", "GEorgewill", "playing games", "Watching Animes", "Programming", "Studying");
+
+// Class work
+console.log("\n Class work \n");
+
+//1
+function unLimited(...limited){
+   console.log(limited);
+}
+
+unLimited("GAMES", "FISH", "BOYS","Movies","laptops", "Money");
